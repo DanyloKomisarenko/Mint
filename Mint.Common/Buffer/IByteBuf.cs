2d03105d4e9772a046c8eb43fb8395a80341cdb2
Dankom <62705242+DanyloKomisarenko@users.noreply.github.com>
2023-02-22 09:57:54 -0500

    IByteBuf documentation

--- Mint.Common/Buffer/IByteBuf.cs
+++ Mint.Common/Buffer/IByteBuf.cs
@@ -1,5 +1,27 @@
 ﻿namespace Mint.Common.Buffer;
 
+/// <summary>
+/// This class allows easier access and manipulation to bytes.
+/// </summary>
+/// This class partitions data into three sections:
+/// Readable Bytes:
+/// This section spans between the reader index and the writer 
+/// index. It contains the bytes that can be read. When initialized 
+/// it is usually zero.
+/// Writable Bytes:
+/// This section spans between the writer index and the capacity. It 
+/// contains the space that can be written to. When initialized is 
+/// also zero.
+/// Discardable Bytes:
+/// This section spans between the zero index and the reader index. It 
+/// contains the read bytes that are now unreachable unless a flip operation
+/// is executed.
+/// Methods:
+/// Clear:
+/// Resets the reader and writer index to prepare for more write operations.
+/// Flip:
+/// Resets the reader and index and sets the writer index to max. This allows
+/// the reading of the entire buffer even the unwritten bytes.
 public interface IByteBuf
 {
     public int ReaderIndex();
